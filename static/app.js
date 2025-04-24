2a7751f968d9e055fd03c664c46eccf7c2bd2da7
lowiechojas <81959291+lowiechojas@users.noreply.github.com>
2025-04-24 16:08:16 +0800

    attempting to add dynamically the longitude, alttitude, and travelmode which is required on the RouteApi

---
+++ static/app.js
@@ -0,0 +1,25 @@
+function initAutocomplete() {
+    const input = document.getElementById("location-input");
+    const autocomplete = new google.maps.places.Autocomplete(input);
+  
+    autocomplete.addListener("place_changed", function () {
+      const place = autocomplete.getPlace();
+      if (!place.geometry) {
+        alert("No location data found!");
+        return;
+      }
+  
+      const lat = place.geometry.location.lat();
+      const lng = place.geometry.location.lng();
+  
+      // Fill the hidden form inputs
+      document.getElementById("lat").value = lat;
+      document.getElementById("lng").value = lng;
+    });
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    if (window.google && window.google.maps) {
+      initAutocomplete();
+    }
+  });
